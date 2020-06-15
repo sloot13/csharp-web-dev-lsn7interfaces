14a0dff8915e248cd35dc5d23601b93688f97998
Kevin Franklin <deerdance72@gmail.com>
2020-06-15 14:44:48 -0400

    final countdown

---
+++ IceCreamShop/ConeComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class ConeComparer : IComparer<Cone>
+    {
+        public int Compare(Cone x, Cone y)
+        {
+            int num = 0;
+
+            if (x.Cost - y.Cost < 0){ num = -1; }
+            if (y.Cost - x.Cost < 0) { num = 1; }
+
+            return num;
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,17 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            foreach (Cone f in availableCones)
+            {
+                Console.WriteLine(f);
+            }
+            availableFlavors.Sort(new FlavorComparer());
+            availableCones.Sort(new ConeComparer());
+            foreach (Cone f in availableCones)
+            {
+                Console.WriteLine(f);
+            }
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
-
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
-        }
+        }   
     }
 }
